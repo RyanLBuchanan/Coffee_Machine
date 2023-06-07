d2c91df0217ad7be36a4b55995912b3a977a03ea
Ryan Lowell Buchanan <buchanan.ryan22@gmail.com>
2023-06-07 11:00:10 -0600

    feat: Instantiated initial machines

--- oop-coffee-machine-start/main.py
+++ oop-coffee-machine-start/main.py
@@ -1,3 +1,8 @@
 from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+my_coffee_machine = CoffeeMaker()
+
+my_money_machine = MoneyMachine()
+
